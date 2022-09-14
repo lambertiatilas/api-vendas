2e9d277a7cdf8b769616b363080b1bcb93835f6a
Atilas Rodrigues Lamberti <lambertiatilas@gmail.com>
2022-09-13 23:12:52 -0300

    Criando os arquivos controller do módulo produto

---
+++ src/modules/products/services/DeleteProductService.ts
@@ -0,0 +1,23 @@
+import AppError from '@shared/errors/AppError';
+import { ProductRepository } from '../typeorm/repositories/ProductsRepository';
+import { getCustomRepository } from 'typeorm';
+
+interface IRequest {
+  id: string;
+}
+
+class DeleteProductService {
+  public async execute({ id }: IRequest): Promise<void> {
+    const productsRepository = getCustomRepository(ProductRepository);
+
+    const product = await productsRepository.findOne(id);
+
+    if (!product) {
+      throw new AppError('Product not found.');
+    }
+
+    productsRepository.remove(product);
+  }
+}
+
+export default DeleteProductService;
